148bec33259e471cf6d65f455a71ceef4a775806
lishaozhan <2351221785@qq.com>
2017-07-04 20:18:52 +0800

    李少展修改

--- obj/routes/index.js
+++ obj/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/redirect', function(req, res, next) {
+    res.redirect('/found');
+});
+
+
+router.get('/found',function (req,res,next){
+    res.render('found',{message:req.query.message});
 });
 
 module.exports = router;
